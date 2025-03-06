5a7a21d5ca865919b1ee1703f71426ec22e9b7c6
JANGHWAN <nwan0947@gmail.com>
2025-03-06 22:05:05 +0900

    2025.03.06 (Print by combining string / Print string one by one)

---
+++ CodingTest/7_Print By_Combining_String.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest
+{
+    class _7_Print_By_Combining_String
+    {
+        static void Main(string[] args)
+        {
+            // 두개의 문자열을 한번에 입력 받아 배열에 저장하기 위한 배열 선언
+            string[] str;
+
+            // 한번 정리
+            Console.Clear();
+
+            //입력 받은 두개의 문자열은 공백으로 구분 되기 때문에 split() 메소드를 사용해서 각 배열에 저장
+            str = Console.ReadLine().Split(' ');
+
+            // 제약 조건
+            if( 1 <= str[0].Length && str[0].Length <= 10 && 1 <= str[1].Length && str[1].Length <= 10)
+            {
+                // 출력
+                Console.WriteLine($"{str[0]}{str[1]}");
+            }
+        }
+    }
+}
---
+++ CodingTest/8_Print_String_One_by_One.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest
+{
+    class _8_Print_String_One_by_One
+    {
+        static void Main(string[] args)
+        {
+            // 입력 받은 값을 저정하기 위한 변수 선언
+            string s;
+
+            // 문자열을 입력 받아 변수 초기화
+            s = Console.ReadLine();
+
+            // 제약 조건
+            if(1 <= s.Length && s.Length <= 10)
+            {
+                // 해당 문자열을 한번 순회
+                foreach (char a in s)
+                {
+                    // 출력
+                    Console.WriteLine(a);
+                }
+            }
+        }
+    }
+}
